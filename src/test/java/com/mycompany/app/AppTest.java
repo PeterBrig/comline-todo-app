f83597eb4385c5977e249196ffc639b39101a51c
PeterBrig <brigadirsti@gmail.com>
2022-04-06 14:55:18 +0300

    switched to maven

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple xxx.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
